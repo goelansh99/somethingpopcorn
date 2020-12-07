76726125df33656ba54e2165206cd7aec1f807ae
Advay S Pethkar <42491924+dot9aitclass@users.noreply.github.com>
2020-12-07 18:30:09 +0530

    Update imdbreq.py

--- imdbreq.py
+++ imdbreq.py
@@ -6,8 +6,8 @@ url = "https://imdb8.p.rapidapi.com/title/auto-complete"
 querystring = {"q":"tron"}
 
 headers = {
-    'x-rapidapi-key': "390eb3d922msh2c82861713f4c28p1ee060jsnd27953c51187",
-    'x-rapidapi-host': "imdb8.p.rapidapi.com"
+    'x-rapidapi-key': "",
+    'x-rapidapi-host': ""
     }
 
 response = requests.request("GET", url, headers=headers, params=querystring)
