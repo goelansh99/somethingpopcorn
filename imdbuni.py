49ab2ea075f594b85d1f5fe99f53ae28e59b9453
Advay S Pethkar <42491924+dot9aitclass@users.noreply.github.com>
2020-12-07 18:29:32 +0530

    Update imdbuni.py

--- imdbuni.py
+++ imdbuni.py
@@ -3,8 +3,8 @@ import http.client
 conn = http.client.HTTPSConnection("imdb8.p.rapidapi.com")
 
 headers = {
-    'x-rapidapi-key': "390eb3d922msh2c82861713f4c28p1ee060jsnd27953c51187",
-    'x-rapidapi-host': "imdb8.p.rapidapi.com"
+    'x-rapidapi-key': "",
+    'x-rapidapi-host': ""
     }
 
 conn.request("GET", "/title/find?q=game%20of%20thr", headers=headers)
